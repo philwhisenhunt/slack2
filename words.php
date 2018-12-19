0faa6978bc47196fbecb2e01b2b7796252d9811c
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-19 12:21:29 -0500

    Building rough first version of word deleter

---
+++ words.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <script src="" async defer></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>
+        <script>
+
+
+            
+            $(document).ready(function(){
+                $("span").click(function(){
+                    $(this).hide();
+                });
+            });
+    
+        </script>
+
+        <h3>Click the words to remove them</h3>
+
+        <?php
+            $paragraph = "Text goes here. It can be as long as needed.";
+            $splitIntoWords = explode(" ", $paragraph);
+            // print_r($splitIntoWords);
+
+            $myArray = ['word', 'dance', 'play'];
+
+            foreach ($splitIntoWords as $entry){
+                echo "<span> $entry" . " " . "</span>";
+            }
+        ?>
+    </body>
+</html>
