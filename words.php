76f1c54b46ed0122ef1a3b58c6fb02bec32359a7
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2018-12-20 13:45:57 -0500

    Added CSS and refined textarea

--- words.php
+++ words.php
@@ -1,10 +1,16 @@
 <!DOCTYPE html>
+
+<head>
+<link rel="stylesheet" type="text/css" href="master.css">
+
+</head>
     <body>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
         
         <script src="" async defer></script>
+        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>
         <script>
 
@@ -16,7 +22,7 @@
                     data: "chatMessage=" + theVar,
 
                     success: function(response) {
-                        console.log(response + " is the pasted text");
+                        // console.log(response + " is the pasted text");
                         $("#displayedText").html(response);                       
 
                     }
@@ -26,24 +32,26 @@
             });
 
             $(document).ready(function(){
-                $("span").click(function(){
-                    $(this).hide();
+                $("#displayedText").click(function(e){
+                    $(e.target).hide();
+                    console.log(e);
+                    // $(this).hide();
                 });
             });
     
         </script>
-
-        <input type="text" name="pastedText" id="thePastedStuff">
+        <h2>Paste in your favorite passage here then click submit: </h2>
+        <textarea name="pastedText" id="thePastedStuff"></textarea>
         <button type = "submit" id="daButton">Let's go</button>
 
         <h3>Don't like what you see? Click any of the words below to remove them</h3>
             
-      
+            <h4>
                 <p id="displayedText">
                  
                  </p>
          
-       
+            </h4>
 
 
             </div>  
